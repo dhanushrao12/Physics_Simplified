fa9d7e8d0893c6fb95842056d8bec44795cdb27c
dhanushrao12 <dhanusharao12@gmail.com>
2022-06-11 14:01:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+
+//Middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+
+//View engine
+app.set("view engine", "ejs");
+
+const PORT = process.env.PORT || 3000;
+//Database connection
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log("Connected to database");
+    app.listen(PORT);
+    console.log(`Listening on http://localhost:${PORT}`);
+  })
+  .catch((err) => console.log(err));
+
+//Routes
+app.get("/", (req, res) => res.render("home"));
+app.get("/courses", (req, res) => res.render("courses"));
+app.use(authRoutes);
