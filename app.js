ba5e6a2aef83dc56141afe650e87f9ef690b9a81
dhanushrao12 <dhanusharao12@gmail.com>
2022-06-14 16:57:21 +0530

    final commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+const express = require("express");
+const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 const cookieParser = require("cookie-parser");
 const { requireAuth, checkUser } = require("./middleware/authMiddleware");
